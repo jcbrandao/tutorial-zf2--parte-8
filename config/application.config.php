f9853c16dfe27b1a3f754c035b38be9952abf9a9
igortice <igortice@gmail.com>
2013-10-15 18:57:30 -0300

    Zend Developer Tools add

--- config/application.config.php
+++ config/application.config.php
@@ -7,7 +7,8 @@
  */
 return array(
     'modules' => array(
-        'Contato'
+        'Contato',
+        'ZendDeveloperTools',
         ),
     'module_listener_options' => array(
         'module_paths' => array(

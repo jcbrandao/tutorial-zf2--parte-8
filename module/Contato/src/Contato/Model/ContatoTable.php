b734c4f809a76331bf1da92e2f3362ce3dd64611
igortice <igortice@gmail.com>
2013-10-21 20:43:58 -0300

    add metodo find contato table

--- module/Contato/src/Contato/Model/ContatoTable.php
+++ module/Contato/src/Contato/Model/ContatoTable.php
@@ -26,12 +26,30 @@ class ContatoTable
     }
 
     /**
-     * Recuperar todos os elementos da tabela
+     * Recuperar todos os elementos da tabela contatos
      * 
      * @return ResultSet
      */
     public function fetchAll()
     {
         return $this->tableGateway->select();
+    }
+    
+    /**
+     * Localizar linha especifico pelo id da tabela contatos
+     * 
+     * @param type $id
+     * @return \Model\Contato
+     * @throws \Exception
+     */
+    public function find($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row)
+            throw new \Exception("Não foi encontrado contado de id = {$id}");
+        
+        return $row;
     }
 }
